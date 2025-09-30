ca68dc796eaa8b603edf2290442799f0980baa29
mahmoudmohamedsh <201727100@std.sci.cu.edu.eg>
2025-09-30 09:28:06 +0300

    make auth use query

--- src/api/getUserDataFromCookies.js
+++ src/api/getUserDataFromCookies.js
@@ -2,17 +2,18 @@ import useUserStore from "@/store/userStore";
 import { BASE_URL } from "@/utils/constants";
 import axios from "axios";
 
-
 export const getUserDataFromCookies = async () => {
+  try {
+    const res = await axios.post(
+      `${BASE_URL}checkCookies`,
+      {},
+      {
+        withCredentials: true,
+      }
+    );
 
-    try {
-        const res = await axios.post(`${BASE_URL}checkCookies`, {}, {
-            withCredentials: true
-        })
-
-        console.log("data from getUserDataFromCookies", res)
-        return res.data.user
-    } catch (error) {
-        throw new Error(error.message)
-    }
-}
+    return res.data.user;
+  } catch (error) {
+    throw error;
+  }
+};
--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,32 +1,16 @@
-"use client"
-import React, { useEffect } from 'react';
+"use client";
+import React, { useEffect } from "react";
 import { HomePage } from "./HomePage/HomePage";
-import { getUserDataFromCookies } from '@/api/getUserDataFromCookies';
-import useUserStore from '@/store/userStore';
+import { useGetAuthUser } from "@/hooks/useGetAuthUser";
 
 const Page = () => {
-    const { setUser, } = useUserStore();
+  const { data } = useGetAuthUser();
 
-    const getUserData = async () => {
-        try {
-            const user = await getUserDataFromCookies()
-            setUser(user)
-        } catch (error) {
-            if (process.env.NODE_ENV === 'development') {
-                console.error('get user from cookies error', error);
-            }
-        }
-    }
-
-    useEffect(() => {
-        getUserData()
-    }, [])
-
-    return (
-        <div style={{ overflowX: "hidden !important" }}>
-            <HomePage />
-        </div>
-    );
+  return (
+    <div style={{ overflowX: "hidden !important" }}>
+      <HomePage />
+    </div>
+  );
 };
 
 export default Page;
